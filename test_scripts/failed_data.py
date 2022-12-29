44abb424ac4a8748dc176c661bdb11bfcaa67396
JiaenLiu <jiaen.liu@efrei.net>
2022-12-29 09:57:20 +0100

    test_script

---
+++ test_scripts/failed_data.py
@@ -0,0 +1,13 @@
+# Create a streamlit app that shows the data that failed to be processed
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+
+
+# Load the data
+df = pd.read_csv("/Users/liujiaen/Documents/Text_Recognition/final_project/test_scripts/false.csv")
+
+# Show the data
+st.write(df)
